8ffbb4e99285a42c7cf4825b4bf450dce9389167
Anthony Zhang <anthony_zhang1234@berkeley.edu>
2021-08-16 06:35:19 -0500

    Created all the files needed for the model

--- spammer.py
+++
@@ -1,4 +0,0 @@
-import sys
-import numpy as np
-a1 = np.array([1,2,3,4])
-print(a1)
--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import torch
+import torch.nn as nn
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 a = np.array([1, 2, 3])
-b = torch.rand(3)
-sns.lineplot(x = a, y = b)
+b = torch.rand(2)
+plt.plot(x = a, y = b)
+plt.show()
